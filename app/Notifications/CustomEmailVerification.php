af40d5bf1583345ab89043bed95aaa43fe9663d1
AutocorrectGuy <103662175+AutocorrectGuy@users.noreply.github.com>
2023-10-01 04:27:34 +0200

    Email verification enabled, configured, custom email template made

---
+++ app/Notifications/CustomEmailVerification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\URL;
+
+class CustomEmailVerification extends Notification
+{
+  use Queueable;
+
+  /**
+   * Create a new notification instance.
+   */
+  public function __construct()
+  {
+    //
+  }
+
+  /**
+   * Get the notification's delivery channels.
+   *
+   * @return array<int, string>
+   */
+  public function via(object $notifiable): array
+  {
+    return ['mail'];
+  }
+
+  /**
+   * Get the mail representation of the notification.
+   */
+  public function toMail(object $notifiable): MailMessage
+  {
+    $actionUrl = URL::temporarySignedRoute(
+      'verification.verify',
+      now()->addMinutes(15),
+      [
+        'id' => $notifiable->getKey(),
+        'hash' => sha1($notifiable->getEmailForVerification())
+      ]
+    );
+
+    return (new MailMessage)
+      ->subject(env('APP_NAME') . " email verification")
+      ->view('emails.custom-verification', compact('notifiable', 'actionUrl'));
+  }
+
+  /**
+   * Get the array representation of the notification.
+   *
+   * @return array<string, mixed>
+   */
+  public function toArray(object $notifiable): array
+  {
+    return [
+      //
+    ];
+  }
+}
