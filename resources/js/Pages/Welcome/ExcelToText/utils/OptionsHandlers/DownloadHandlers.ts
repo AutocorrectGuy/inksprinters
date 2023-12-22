7b4ffcd0f709e08b9dddb92a2b7b07ce5fb92dd8
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2023-12-22 01:11:11 +0100

    3rd try to implement certain text-encoding

--- resources/js/Pages/Welcome/ExcelToText/utils/OptionsHandlers/DownloadHandlers.ts
+++ resources/js/Pages/Welcome/ExcelToText/utils/OptionsHandlers/DownloadHandlers.ts
@@ -1,5 +1,7 @@
 import { TextEncoder } from 'text-encoding'
 import { TextEncodingType } from '../TextEditorSettings'
+import { saveAs } from 'file-saver';
+
 
 // export const handleDownload = (inputText: string, encoding: TextEncodingType) => {
 //   try {
@@ -24,27 +26,17 @@ import { TextEncodingType } from '../TextEditorSettings'
 //   }
 // }
 
-export const handleDownload = (inputText: string, encoding: string) => {
+export const handleDownload = (inputText: string, encoding: TextEncodingType) => {
   try {
-    // Convert the string to a binary format in the desired encoding
-    const binaryStr = unescape(encodeURIComponent(inputText));
-    
-    // Create an array of character codes from the binary string
-    const charCodes = new Uint8Array(binaryStr.length);
-    for (let i = 0; i < binaryStr.length; i++) {
-      charCodes[i] = binaryStr.charCodeAt(i);
-    }
+    // Encode the string with the specified encoding
+    const encoder = new TextEncoder(encoding, { NONSTANDARD_allowLegacyEncoding: true });
+    const bytes = encoder.encode(inputText);
 
-    // Create a Blob from the character codes
-    const blob = new Blob([charCodes], { type: `text/plain;charset=${encoding}` });
+    // Create a Blob from the bytes
+    const blob = new Blob([bytes],);
 
-    // Create a download link and trigger the download
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = `names-list-${encoding}.txt`; // Name of the downloaded file
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    // Use FileSaver to save the file
+    saveAs(blob, 'file.txt');
   } catch (error) {
     console.error('Error during file download:', error);
   }
