c890bc20980673afc381d08d6e4c6322ceb1fa30
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2024-01-11 23:27:22 +0100

    Implemented Breadcrumb (fixed)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your React App</title>
+    <title>Inksprinters</title>
     <!-- Include the main React CSS file -->
-    <link href="{{ asset('reactapp/static/css/main.46c0e85f.css') }}" rel="stylesheet">
+    <link href="{{ asset('reactapp/static/css/main.80a6b847.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="root"></div> <!-- React will attach to this div -->
 
     <!-- Include the main React JavaScript file -->
-    <script src="{{ asset('reactapp/static/js/main.3cd334da.js') }}"></script>
+    <script src="{{ asset('reactapp/static/js/main.3fb54542.js') }}"></script>
 </body>
 </html>
