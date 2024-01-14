eddad2f16d389bdf7a066f105c5b32cfe36089ed
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2024-01-14 23:34:33 +0100

    BreadCrumb now can use multi-step components

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inksprinters</title>
     <!-- Include the main React CSS file -->
-    <link href="{{ asset('reactapp/static/css/main.80a6b847.css') }}" rel="stylesheet">
+    <link href="{{ asset('reactapp/static/css/main.fe57d07c.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="root"></div> <!-- React will attach to this div -->
 
     <!-- Include the main React JavaScript file -->
-    <script src="{{ asset('reactapp/static/js/main.3fb54542.js') }}"></script>
+    <script src="{{ asset('reactapp/static/js/main.dff78186.js') }}"></script>
 </body>
 </html>
