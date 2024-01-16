a36433fe5bac7fca670448cc2d02ff30d74c798f
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2024-01-17 00:59:07 +0100

    Refreshing page (404) error fixed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inksprinters</title>
     <!-- Include the main React CSS file -->
-    <link href="{{ asset('reactapp/static/css/main.0f485c2e.css') }}" rel="stylesheet">
+    <link href="{{ asset('reactapp/static/css/main.59eb2bbc.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="root"></div> <!-- React will attach to this div -->
 
     <!-- Include the main React JavaScript file -->
-    <script src="{{ asset('reactapp/static/js/main.bb572b74.js') }}"></script>
+    <script src="{{ asset('reactapp/static/js/main.bc9862f8.js') }}"></script>
 </body>
 </html>
