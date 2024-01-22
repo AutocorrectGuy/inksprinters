ecaaf2dd187be0ca4903efe75b434b0aa2261b00
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2024-01-22 23:14:54 +0100

    Templating CRUD components

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inksprinters</title>
     <!-- Include the main React CSS file -->
-    <link href="{{ asset('static/css/main.c14fd8e6.css') }}" rel="stylesheet">
+    <link href="{{ asset('static/css/main.252c8e3b.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="root"></div> <!-- React will attach to this div -->
 
     <!-- Include the main React JavaScript file -->
-    <script src="{{ asset('static/js/main.fecb09f2.js') }}"></script>
+    <script src="{{ asset('static/js/main.62976a25.js') }}"></script>
 </body>
 </html>
