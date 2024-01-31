b6b1c4bc0b893ec105abe6b66b2a4e891606010e
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2024-01-31 03:36:42 +0100

    crud refactored

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inksprinters</title>
     <!-- Include the main React CSS file -->
-    <link href="{{ asset('static/css/main.252c8e3b.css') }}" rel="stylesheet">
+    <link href="{{ asset('static/css/main.3b9b82e5') }}" rel="stylesheet">
 </head>
 <body>
     <div id="root"></div> <!-- React will attach to this div -->
 
     <!-- Include the main React JavaScript file -->
-    <script src="{{ asset('static/js/main.7e63b142.js') }}"></script>
+    <script src="{{ asset('static/js/main.f40ae3b6.js') }}"></script>
 </body>
 </html>
